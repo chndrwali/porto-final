68fd5eca10b144740f0b374ebf791931fa2d51b8
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-01-14 16:01:20 +0700

    feat(auth): adding auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
+import { LoginForm } from '@/components/auth/loginForm';
+
 const Page = () => {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold text-white">Masuk</h1>
       <p className="text-muted-foreground">Gunakan email dan password yang valid untuk mendapatkan akses ke Admin</p>
+      <LoginForm />
     </div>
   );
 };
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,28 @@
+'use client';
+
+import { logout } from '@/actions/logout';
+import { Button } from '@/components/ui/button';
+import { useConfirm } from '@/hooks/use-confirm';
+import { toast } from '@/hooks/use-toast';
+
 export default function Home() {
-  return <> </>;
+  const [ConfirmDialog, confirm] = useConfirm('Apakah kamu yakin?', 'Anda akan keluar akun.');
+  const handleLogout = async () => {
+    const ok = await confirm();
+    if (ok) {
+      logout();
+      toast({
+        title: 'Berhasil',
+        description: 'Berhasil keluar akun',
+        variant: 'success',
+      });
+    }
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Button onClick={handleLogout}>Logout</Button>{' '}
+    </>
+  );
 }
