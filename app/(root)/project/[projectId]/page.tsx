1cf7c10a5bf9a25e983aceed8b1c0e8298268007
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-01-18 17:35:46 +0700

    feat: finish

--- app/(root)/project/[projectId]/page.tsx
+++ app/(root)/project/[projectId]/page.tsx
@@ -1,5 +1,22 @@
-const Page = () => {
-  return <></>;
+import { getProjectById } from '@/actions/getProjectById';
+import DetailProject from '@/components/root/detailProject';
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+interface Props {
+  params: Promise<{ projectId: string }>;
+}
+
+export const metadata: Metadata = {
+  title: 'Detail Projects',
+};
+
+const Page = async ({ params }: Props) => {
+  const { projectId } = await params;
+  const project = await getProjectById(projectId);
+
+  if (!project) redirect('/project');
+  return <DetailProject project={project} />;
 };
 
 export default Page;
