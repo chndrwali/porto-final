59183d25a094a0fdd7b1537d1071a3d407563fd9
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2025-01-28 21:06:50 +0700

    fix

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -28,7 +28,7 @@ export const TextGenerateEffect = ({ words, className }: { words: string; classN
             <motion.span
               key={word + idx}
               // change here if idx is greater than 3, change the text color to #CBACF9
-              className={` ${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} opacity-0`}
+              className={` ${idx > 3 ? 'text-purple' : 'text-white'} opacity-0`}
             >
               {word}{' '}
             </motion.span>
@@ -43,7 +43,7 @@ export const TextGenerateEffect = ({ words, className }: { words: string; classN
       {/* mt-4 to my-4 */}
       <div className="my-4">
         {/* remove  text-2xl from the original */}
-        <div className=" dark:text-white text-black leading-snug tracking-wide">{renderWords()}</div>
+        <div className=" text-white leading-snug tracking-wide">{renderWords()}</div>
       </div>
     </div>
   );
